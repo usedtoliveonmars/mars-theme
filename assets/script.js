2b6e486ed916c00f3642ed066e6109cfae7aab07
chris <chris.cushman@ymail.com>
2017-12-29 22:33:22 -0500

    added display, changed h tags to cascade

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,12 @@
-$("button").click(function(){
-    $("#contactform").toggle();
-});
+$(function() {
+       $('#hide').click(function() {
+                $('td:nth-child(3)').hide();                
+       });
+
+    $('#show').click(function() {
+                $('td:nth-child(3)').show();                
+       });
+    $('#toggle').click(function() {
+                $('td:nth-child(3)').toggle();                
+       });
+	});
